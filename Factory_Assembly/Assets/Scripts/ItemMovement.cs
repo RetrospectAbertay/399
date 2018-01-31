462f9a27223b2137819d832039d9ef2782272186
Jordan Ross <jordan-ross-93@hotmail.co.uk>
2018-01-31 02:05:05 +0000

    Added the spawning and movement scripts Has multi display Added placeholder belts and prefabs

---
+++ Factory_Assembly/Assets/Scripts/ItemMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMovement : MonoBehaviour {
+
+    // conveyer belt variables
+    float _beltForceX;
+    float _beltForceY;
+    public float _beltForce;
+
+    public bool isUp;
+    public bool isLeft;
+
+    void Start()
+    {
+        switch (isUp)
+        {
+            case false:
+                _beltForceY = _beltForce;
+                break;
+           /* case 2:
+                _beltForceY = -_beltForce;
+                break;
+            case 3:
+                _beltForceX = -_beltForce;
+                break;*/
+            default:
+                _beltForceX = _beltForce;
+                break;
+        }
+
+       /* if (itemType == WorkBench.SpectrumPart.FinishedSpectrum)
+        {
+            Debug.Log("box has spawned!");
+        }*/
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Belt")
+        {
+            //_onBelt = true;
+            this.transform.position = new Vector3(this.transform.position.x + _beltForceX, this.transform.localPosition.y + _beltForceY, this.transform.position.z);
+        }
+    }
+    
+}
---
+++ Factory_Assembly/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+
+    public Transform[] spawnPoint;
+    public GameObject[] spawnPrefab;
+    public GameObject[] spawnClone;
+    public float MaxTime;
+    private float timer;
+
+    void Start()
+    {
+        timer = MaxTime;
+    }
+
+    void Update()
+    {
+        timer -= (10 * Time.deltaTime);
+
+        if (timer <= 0)
+        {
+            SpawnItem();
+            timer = MaxTime;
+        }
+    }
+
+    void SpawnItem()
+    {
+        spawnClone[0] = Instantiate(spawnPrefab[Random.Range(0, 3)], spawnPoint[0].transform.position, Quaternion.Euler(0, 0, 0));
+    }
+    void Timer()
+    {
+
+    }
+
+}
