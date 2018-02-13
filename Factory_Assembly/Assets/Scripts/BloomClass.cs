d6670c2c5182d9c34de544712b3e0ce3c8b147cb
Jordan Ross <jordan-ross-93@hotmail.co.uk>
2018-02-13 17:04:45 +0000

    Add the new textures

---
+++ Factory_Assembly/Assets/Scripts/BloomClass.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[ExecuteInEditMode]
+public class BloomClass : MonoBehaviour
+{
+
+    public Material material;
+    // Use this for initialization
+    void Start()
+    {
+        material = new Material(Shader.Find("Sprites/ItemGlow"));
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        material.SetTexture("_MainTex", source);
+        Graphics.Blit(source, destination, material);
+    }
+}
