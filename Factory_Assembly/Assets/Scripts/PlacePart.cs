1caf0a0c25e4f31a93434811ce42a8ca3edc8565
Unknown <1500742@uad.ac.uk>
2018-02-23 22:44:23 +0000

    First Control working with bottom prefab

--- Factory_Assembly/Assets/Scripts/PlacePart.cs
+++ Factory_Assembly/Assets/Scripts/PlacePart.cs
@@ -7,11 +7,12 @@ using UnityEngine;
 public class PlacePart : MonoBehaviour {
     public bool scorePoint;
     public int scoreValue;
-    public Rigidbody2D[] part;
+    public Rigidbody2D part;
 
 	// Use this for initialization
     void Start () {
-		
+		part = GetComponent<Rigidbody2D>();
+			
 	}
 	
 	// Update is called once per frame
@@ -19,23 +20,27 @@ public class PlacePart : MonoBehaviour {
 		
 	}
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D coll)
     {
-        
+		//Debug.Log("triggers");
         /*for (int i = 0; i < partGo.Length; i++)
         {
             Debug.Log("Triggered!");
             DestroyImmediate(partGo[i].gameObject, true);
         }*/
-        if (other.gameObject.tag == "Parts")
-        {
-            Debug.Log("j");
-            if (Input.GetKeyDown("a"))
-            {
-                Debug.Log("a");
+		if (coll.gameObject.tag == "Finish")
+		{
+			Debug.Log ("Finish");
+			 if(Input.GetKeyDown("space"))
+        	{
+            Debug.Log("part");
+			part.constraints = RigidbodyConstraints2D.FreezePositionX;
+            //if (Input.GetKeyDown("a"))
+          //  {
+              //  Debug.Log("a");
                 // Destroy(this.gameObject, 0);
                 //Destroy (other.gameObject, 0);
-                part[0].velocity = new Vector2(part[0].position.x, part[0].position.y + 40) * Time.fixedDeltaTime;
+            //    part[0].velocity = new Vector2(part[0].position.x, part[0].position.y + 40) * Time.fixedDeltaTime;
 
                 if (scorePoint == true)
                 {
