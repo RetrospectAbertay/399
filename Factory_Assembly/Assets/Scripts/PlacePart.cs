3217b4e0bdb37c092161a36ef02b1013ea408159
Matthew Wallace <1502616@abertay.ac.uk>
2018-03-04 16:49:44 +0000

    cherry-pick conflict resolved

--- Factory_Assembly/Assets/Scripts/PlacePart.cs
+++ Factory_Assembly/Assets/Scripts/PlacePart.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class PlacePart : MonoBehaviour {
     public bool scorePoint;
+    public int scoreValue;
     //public Rigidbody2D part;
 
 	// Use this for initialization
@@ -18,6 +19,7 @@ public class PlacePart : MonoBehaviour {
 	void Update () {
 		
 	}
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(scorePoint== true)
@@ -68,4 +70,47 @@ public class PlacePart : MonoBehaviour {
     //		}
     //	} 
     //	
+
+//    void OnTriggerStay2D(Collider2D coll)
+//    {
+//		//Debug.Log("triggers");
+//        /*for (int i = 0; i < partGo.Length; i++)
+//        {
+//            Debug.Log("Triggered!");
+//            DestroyImmediate(partGo[i].gameObject, true);
+//        }*/
+//		if (coll.gameObject.CompareTag("Finish") &&Input.GetKeyDown ("space") )
+//		{
+//				Debug.Log ("one");
+//				part.constraints = RigidbodyConstraints2D.FreezePositionX;
+//
+//
+//        }
+//
+//		if (coll.gameObject.CompareTag ("stop"))
+//		{
+//
+//				Debug.Log ("two");
+//				part.constraints = RigidbodyConstraints2D.FreezePositionY;
+//				part.constraints = RigidbodyConstraints2D.FreezePositionX;
+//
+//			if (Input.GetKeyDown ("z")) {
+//
+//				part.constraints = RigidbodyConstraints2D.FreezePositionX = false;
+//			}
+//
+//				//if (Input.GetKeyDown("a"))
+//				//  {
+//				//  Debug.Log("a");
+//				// Destroy(this.gameObject, 0);
+//				//Destroy (other.gameObject, 0);
+//				//    part[0].velocity = new Vector2(part[0].position.x, part[0].position.y + 40) * Time.fixedDeltaTime;
+//
+//				if (scorePoint == true) {
+//					Score.score += scoreValue;
+//				}
+//
+//		}
+//	} 
+//	
 }
